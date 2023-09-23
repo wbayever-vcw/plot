97b6c360e979f18be3008d2cf90b2386f3c7b161
Philippe Rivière <fil@rezo.net>
2023-09-23 18:33:03 +0200

    we need to know if it's y or fy, to apply a margin or facet margin

--- src/mark.js
+++ src/mark.js
@@ -66,9 +66,9 @@ export class Mark {
     this.dx = +dx;
     this.dy = +dy;
     this.marginTop = +marginTop;
-    this.marginRight = marginRight === "auto" ? "auto" : +marginRight;
+    this.marginRight = marginRight === "y" || marginRight === "fy" ? marginRight : +marginRight;
     this.marginBottom = +marginBottom;
-    this.marginLeft = marginLeft === "auto" ? "auto" : +marginLeft;
+    this.marginLeft = marginLeft === "y" || marginLeft === "fy" ? marginLeft : +marginLeft;
     this.clip = maybeClip(clip);
     this.tip = maybeTip(tip);
     // Super-faceting currently disallow position channels; in the future, we
